1078ccec565c2e5cba4626b5fe34d6b4b95abf2b
javicaib <javicaib.mail@gmail.com>
2022-08-09 05:45:28 +0200

    Serializadores actualizados y metodo Detail

--- Store/Products/serializer.py
+++ Store/Products/serializer.py
@@ -3,10 +3,11 @@ from .models import Product,Category
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Product
-        exclude = ('state',)
+        exclude = ('state','created_at','update_at')    
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        exclude = ('state',)        
+        exclude = ('state','created_at','update_at')       
--- Store/Products/viewsets.py
+++ Store/Products/viewsets.py
@@ -1,8 +1,10 @@
+from itertools import product
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from .models import Product
-from .serializer import ProductSerializer,CategorySerializer
+from .models import Category, Product
+from .serializer import ProductSerializer, CategorySerializer
+from django.shortcuts import get_object_or_404
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -27,6 +29,20 @@ class ProductViewSet(viewsets.ModelViewSet):
             return Response({'message': 'Producto añadido correctamente'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def retrieve(self, request, pk=None):
+
+        if pk == 'by-category':
+            return Response({'error': 'Debe indicar un id valido'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        product = self.get_queryset(pk)
+        
+        if product:
+            serializer = self.serializer_class(product)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'No se encuentra el producto'}, status=status.HTTP_400_BAD_REQUEST)
+
     def update(self, request, pk=None):
         if self.get_queryset(pk):
             serializer = self.serializer_class(
@@ -35,26 +51,27 @@ class ProductViewSet(viewsets.ModelViewSet):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-       
-    def destroy(self,resquest,pk=None):
-        product = self.get_queryset().filter(id = pk).first()
+
+    def destroy(self, resquest, pk=None):
+        product = self.get_queryset().filter(id=pk).first()
         if product:
             product.state = False
             product.save()
-            return  Response({'message': 'Producto eliminado correctamente'}, status=status.HTTP_200_OK)
-        return Response({'error': 'No se encuentra el producto'},status=status.HTTP_400_BAD_REQUEST)    
-    
-    
-    def product_by_category(self,request,pk=None):
-        queryset = Product.objects.filter(category = pk)
-        serializer = self.get_serializer(queryset, many=True) 
+            return Response({'message': 'Producto eliminado correctamente'}, status=status.HTTP_200_OK)
+        return Response({'error': 'No se encuentra el producto'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def product_by_category(self, request, pk=None):
+        queryset = Product.objects.filter(category=pk)
+        serializer = self.get_serializer(queryset, many=True)
         if len(serializer.data) == 0:
-            return Response({'error': 'No hay productos en con el id de la categoria proporcionada'},status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.data,status=status.HTTP_200_OK)    
+            return Response({'error': 'No hay productos en con el id de la categoria proporcionada'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
 
-    queryset = Product.objects.filter(state=True)
-    serializer_class = CategorySerializer     
-
-  
+    serializer_class = CategorySerializer
+    def get_queryset(self, pk=None):
+        if pk is None:
+            return Category.objects.filter(state=True)
+        return Category.objects.filter(id=pk, state=True).first()
