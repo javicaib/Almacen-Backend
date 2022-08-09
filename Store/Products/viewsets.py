82b7725f1f0a23c98d68bd6ba3331cd1f13a5234
javicaib <javicaib.mail@gmail.com>
2022-08-09 04:30:22 +0200

    quitado imports innecesarios

--- Store/Products/viewsets.py
+++ Store/Products/viewsets.py
@@ -1,5 +1,3 @@
-from unicodedata import category
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
