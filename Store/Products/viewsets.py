87209afc283adc49b04ef700b8559a1c55efca7c
javicaib <javicaib.mail@gmail.com>
2022-08-09 04:28:59 +0200

    message ->error

--- Store/Products/viewsets.py
+++ Store/Products/viewsets.py
@@ -51,7 +51,7 @@ class ProductViewSet(viewsets.ModelViewSet):
         queryset = Product.objects.filter(category = pk)
         serializer = self.get_serializer(queryset, many=True) 
         if len(serializer.data) == 0:
-            return Response({'message': 'No hay productos en con el id de la categoria proporcionada'},status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'No hay productos en con el id de la categoria proporcionada'},status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.data,status=status.HTTP_200_OK)    
 
 class CategoryViewSet(viewsets.ModelViewSet):
